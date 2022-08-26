d1709afbdbb6884c620868f0cc5204a1f8a2cd62
hitesh saini <hiteshsaini639@gmail.com>
2022-08-26 09:28:49 +0530

    getElementsByClassName

--- main.js
+++ main.js
@@ -28,3 +28,16 @@ let title=document.querySelectorAll('.title');
 header.style.borderBottom='solid 3px #000';
 title[0].style.color='green';
 title[0].style.fontWeight='bold'
+
+let items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent="Hello 2";
+//items[1].style.fontWeight='bold';
+items[1].style.backgroundColor="yellow";
+items[2].style.backgroundColor="green";
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
