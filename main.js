f1413396518f71049e6526ccbbbef2862cb1866e
hitesh saini <hiteshsaini639@gmail.com>
2022-08-26 08:58:54 +0530

    Changed

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 // Examine the document obejct//
-// console.dir(document);
-// console.log(document.domain);
+//console.dir(document);
+//console.log(document.domain);
 // console.log(document.URL);
 // console.log(document.title);
-// // document.title=123;
+// document.title=123;
 // console.log(document.doctype);
-// console.log(document.head);
+//console.log(document.head);
 // console.log(document.body);
 // console.log(document.all);
 // console.log(document.all[10]);
-// // document.all[10].textContent="Hello";
+// document.all[10].textContent="Hello";
 // console.log(document.forms);
 // console.log(document.links);
 // console.log(document.images);
 
 // console.log(document.getElementById('header-title'));
 // let headerTitle = document.getElementById('header-title');
-let header= document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='GoodBye';
+// headerTitle.innerHTML='<h3>HELLO</h3>';
 
-header.style.borderBottom='solid 3px #000';
+
+
+let header= document.getElementById('main-header');
 let title=document.querySelectorAll('.title');
+header.style.borderBottom='solid 3px #000';
 title[0].style.color='green';
-title[0].style.fontWeight='bold';
+title[0].style.fontWeight='bold'
